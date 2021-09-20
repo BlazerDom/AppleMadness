a7c472ef4fa155dc7a17323ebdfc5ca77c30e500
BlazerDom <blazerdom@yandex.ru>
2021-09-20 17:40:36 +0300

    Улучшение таблицу лидеров.  Разделил текстовые поля для номера, имени и очков

--- Assets/Scripts/HSLeaderboard.cs
+++ Assets/Scripts/HSLeaderboard.cs
@@ -33,13 +33,16 @@ public class HSLeaderboard : MonoBehaviour
     {
         for (int i = 0; i != scoreArray.Length; i++)
         {
-            Text t = scoreArray[i].GetComponent<Text>();
+            GameObject nameObject = scoreArray[i].transform.GetChild(1).gameObject;
+            Text n = nameObject.GetComponent<Text>();
+            GameObject scoreObject = scoreArray[i].transform.GetChild(2).gameObject;
+            Text s = scoreObject.GetComponent<Text>();
             var nameScore = PlayerPrefs.GetString(key: hsLevel + $"_{i}");
             var a = nameScore.Split(";".ToCharArray());
             var score = a.Where(x => x.StartsWith("Score:")).Select(a => a.Substring(a.IndexOf(":") + 1)).Single();
             var name = a.Where(x => x.StartsWith("Name:")).Select(x => x.Substring(x.IndexOf(":") + 1)).Single();
-            var j = i + 1;
-            t.text = j.ToString() + ". " + name + "    " + score;
+            n.text = name;
+            s.text = score;
         }
 
     }
